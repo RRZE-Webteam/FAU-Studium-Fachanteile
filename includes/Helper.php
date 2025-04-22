d302ddb491f3bb255915d3640030d1c290151598
Barbara Bothe <barbara.bothe@fau.de>
2025-04-22 12:16:39 +0200

    Bugfix Anteile mit >50% (Large Arc Flag)

--- includes/Helper.php
+++ includes/Helper.php
@@ -13,35 +13,34 @@ class Helper {
      */
     public static function drawPieChart($svgID, $data) {
         $total = array_sum(array_column($data, 'percent'));
-        if ($total > 0) {
-            $radiansPerUnit = (2 * pi()) / $total;
-            $startAngleRadians = 0 - pi() / 2;
+        if ($total <= 0)
+            return '';
 
+        $radiansPerUnit = (2 * pi()) / $total;
+        $startAngleRadians = 0 - pi() / 2;
 
-            $svg = '<svg height="300" width="300" viewBox="0 0 300 300" class="chart" id="' . $svgID . '" aria-hidden="true">';
-            foreach ($data as $item) {
-                $sweepAngleRadians = $item[ 'percent' ] * $radiansPerUnit;
-                $sliceData = [
-                    'id' => $svgID,
-                    'centreX' => 150,
-                    'centreY' => 150,
-                    'startAngleRadians' => $startAngleRadians,
-                    'sweepAngleRadians' => $sweepAngleRadians,
-                    'radius' => 150,
-                    'fillColour' => $item[ 'color' ],
-                    'strokeColour' => '#ffffff',
-                    'label' => $item[ 'share' ],
-                    'percent' => $item[ 'percent' ]
-                ];
-                $svg .= self::drawPieSlice($sliceData);
-                $startAngleRadians += $sweepAngleRadians;
-            }
-
-            $svg .= '<circle r="60" cx="150" cy="150" fill="#FFFFFF" />';
-            $svg .= '</svg>';
-            return $svg;
+        $svg = '<svg height="300" width="300" viewBox="0 0 300 300" class="chart" id="' . $svgID . '" aria-hidden="true">';
+        foreach ($data as $item) {
+            $sweepAngleRadians = $item[ 'percent' ] * $radiansPerUnit;
+            $sliceData = [
+                'id' => $svgID,
+                'centreX' => 150,
+                'centreY' => 150,
+                'startAngleRadians' => $startAngleRadians,
+                'sweepAngleRadians' => $sweepAngleRadians,
+                'radius' => 150,
+                'fillColour' => $item[ 'color' ],
+                'strokeColour' => '#ffffff',
+                'label' => $item[ 'share' ],
+                'percent' => $item[ 'percent' ]
+            ];
+            $svg .= self::drawPieSlice($sliceData);
+            $startAngleRadians += $sweepAngleRadians;
         }
-        return '';
+
+        $svg .= '<circle r="60" cx="150" cy="150" fill="#FFFFFF" />';
+        $svg .= '</svg>';
+        return $svg;
     }
 
     private static function drawPieSlice($settings) {
@@ -51,6 +50,8 @@ class Helper {
         $firstCircumferenceY = $settings['centreY'] + $settings['radius'] * sin($settings['startAngleRadians']);
         $secondCircumferenceX = $settings['centreX'] + $settings['radius'] * cos($settings['startAngleRadians'] + $settings['sweepAngleRadians']);
         $secondCircumferenceY = $settings['centreY'] + $settings['radius'] * sin($settings['startAngleRadians'] + $settings['sweepAngleRadians']);
+        $angleDegrees = rad2deg($settings['sweepAngleRadians']);
+        $largeArcFlag = ($angleDegrees > 180) ? 1 : 0;
 
         // move to centre
         $d .= "M" . $settings['centreX'] . "," . $settings['centreY'] . " ";
@@ -58,7 +59,8 @@ class Helper {
         $d .= "L" . $firstCircumferenceX . "," . $firstCircumferenceY . " ";
         // arc
         // Radius X, Radius Y, X Axis Rotation, Large Arc Flag, Sweep Flag, End X, End Y
-        $d .= "A" . $settings['radius'] . "," . $settings['radius'] . " 0 0,1 " . $secondCircumferenceX . "," . $secondCircumferenceY . " ";
+        $d .= "A" . $settings['radius'] . "," . $settings['radius'] . " 0 " . $largeArcFlag . ",1 " . $secondCircumferenceX . "," . $secondCircumferenceY . " ";
+
         // close path
         $d .= "Z";
 
